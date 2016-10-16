5dc348291d01146143cafc72ec2d72a759d0c39c
WuHarry <harrywhy32@gmail.com>
2016-10-16 12:12:25 +1100

    v0.04 beta 0.05
    bug fix

--- Assets/Script/ScreenResize.cs
+++ Assets/Script/ScreenResize.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class ScreenResize : MonoBehaviour {
-
+    
+    void Start()
+    {
+        Resize();
+    }
     void Resize()
     {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
