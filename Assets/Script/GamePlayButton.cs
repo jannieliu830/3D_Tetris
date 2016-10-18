e5cdf06618b0e794f0a397c9d50937006e1d65a3
WuHarry <harrywhy32@gmail.com>
2016-10-18 13:02:48 +1100

    v0.05.02
    Pause button added.

--- Assets/Script/GamePlayButton.cs
+++ Assets/Script/GamePlayButton.cs
@@ -2,8 +2,12 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class GamePlayButton : MonoBehaviour {
-
+public class GamePlayButton : MonoBehaviour
+{
+    public bool isPaused;
+    public bool isGameOver;
+    private Animator anim;
+    public Texture pause;
     void OnGUI()
     {
         if (GUI.Button(new Rect((Screen.width - 100), (Screen.height * 0.02f), 60, 60), "Menu"))
@@ -13,6 +17,18 @@ public class GamePlayButton : MonoBehaviour {
         if (GUI.Button(new Rect((Screen.width - 190), (Screen.height * 0.02f), 60, 60), "Restart"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (GUI.Button(new Rect((Screen.width * 0.85f), (Screen.height * 0.02f), 60, 60), pause))
+        {
+            isPaused = !isPaused;
+            if (isPaused == true)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
         }
     }
 }
